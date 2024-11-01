b5fdae549d489e469c89143bb264ea33821de1ce
LamcatUK <dan@chillibyte.co.uk>
2024-11-01 13:46:47 +0000

    Update single.php

--- single.php
+++ single.php
@@ -87,8 +87,7 @@ $img = get_the_post_thumbnail(get_the_ID(), 'full', array('class' => 'blog__imag
                 $ids = wp_list_pluck($cats, 'term_id');
                 $r = new WP_Query(array(
                     'category__in' => $ids,
-                    'posts_per_page' => 3,
-                    'post__not_in' => get_the_ID()
+                    'posts_per_page' => 3
                 ));
                 while ($r->have_posts()) {
                     $r->the_post();

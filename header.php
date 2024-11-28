da81b10c3ab20c70511e4aa46b206091560eb5f7
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-11-28 15:40:58 +0000

    Update header.php

--- header.php
+++ header.php
@@ -104,7 +104,7 @@ if (get_field('gtm_property', 'options')) {
     ?>
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe
-            src="https://www.googletagmanager.com/ns.html?id=<?=get_field('ga_property', 'options')?>"
+            src="https://www.googletagmanager.com/ns.html?id=<?=get_field('gtm_property', 'options')?>"
             height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     <?php

b5631687e80f62e2d3b6a082e5e02e7350c68704
ChillibyteUK <dan@chillibyte.co.uk>
2025-05-19 10:11:52 +0000

    hero hierarchy

--- page-templates/blocks/cb_form_hero.php
+++ page-templates/blocks/cb_form_hero.php
@@ -1,18 +1,22 @@
 <?php
-$id = random_str(4);
+$unique_id = random_str( 4 );
 ?>
 <section class="form_hero">
     <div class="container-xl py-md-6 text-center">
         <?php
-        if (get_field('pre_title') ?? null) {
+        if ( get_field( 'pre_title' ) ?? null ) {
             ?>
-        <div class="h3 mb-0 font-weight-medium"><?=get_field('pre_title')?></div>
-            <?php
-        }
+        <h1 class="h3 mb-0 font-weight-medium"><?= wp_kses_post( get_field( 'pre_title' ) ); ?></h1>
+        <h2 class="h1"><?= wp_kses_post( get_field( 'title' ) ); ?></h2>
+			<?php
+        } else {
+			?>
+        <h1><?= wp_kses_post( get_field( 'title' ) ); ?></h1>
+			<?php
+		}
         ?>
-        <h1><?=get_field('title')?></h1>
         <div class="form_hero__form">
-            <input type="text" name="postcode_<?=$id?>" id="postcode_<?=$id?>" placeholder="Enter postcode" autocomplete="off"><button class="button button-sm formbutton">Get Your Free Offer</button>
+            <input type="text" name="postcode_<?= esc_attr( $unique_id ); ?>" id="postcode_<?= esc_attr( $unique_id );?>" placeholder="Enter postcode" autocomplete="off"><button class="button button-sm formbutton">Get Your Free Offer</button>
         </div>
     </div>
 </section>

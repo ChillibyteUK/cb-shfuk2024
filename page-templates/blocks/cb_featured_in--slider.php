294e814750548e70e565d1513b792273b700a5a6
ChillibyteUK <dan@chillibyte.co.uk>
2025-06-06 15:27:35 +0100

    no slider

---
+++ page-templates/blocks/cb_featured_in--slider.php
@@ -0,0 +1,64 @@
+<section class="featured_in py-4">
+    <div class="container-xl featured_in__grid">
+        <div class="splide featuredSplide">
+            <div class="splide__track">
+				<ul class="splide__list">
+					<?php
+					foreach ( get_field( 'featured_logos', 'options' ) as $l ) {
+						?>
+					<li class="splide__slide">
+						<?php
+						echo wp_get_attachment_image(
+							$l,
+							'full',
+							false,
+							array(
+								'width'   => '305',
+								'height'  => '90',
+								'loading' => 'eager',
+							)
+						);
+						?>
+					</li>
+						<?php
+					}
+					?>
+				</ul>
+			</div>
+        </div>
+    </div>
+</section>
+<?php
+add_action(
+	'wp_footer',
+	function () {
+    	?>
+<script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js" nitro-exclude></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css" />
+<script nitro-exclude>
+    document.addEventListener('DOMContentLoaded', function () {
+        new Splide('.featuredSplide', {
+            type: 'loop',
+            autoplay: true,
+            perPage: 5,
+            gap: '20px',
+			arrows: false,
+			pagination: false,
+            breakpoints: {
+				992: {
+					perPage: 4,
+				},
+				768: {
+					perPage: 3,
+				},
+                576: {
+                    perPage: 2,
+                },
+            },
+        }).mount();
+    });
+</script>
+        <?php
+    },
+    9999
+);
--- page-templates/blocks/cb_featured_in.php
+++ page-templates/blocks/cb_featured_in.php
@@ -1,64 +1,25 @@
 <section class="featured_in py-4">
     <div class="container-xl featured_in__grid">
-        <div class="splide featuredSplide">
-            <div class="splide__track">
-				<ul class="splide__list">
-					<?php
-					foreach ( get_field( 'featured_logos', 'options' ) as $l ) {
-						?>
-					<li class="splide__slide">
-						<?php
-						echo wp_get_attachment_image(
-							$l,
-							'full',
-							false,
-							array(
-								'width'   => '305',
-								'height'  => '90',
-								'loading' => 'eager',
-							)
-						);
-						?>
-					</li>
-						<?php
-					}
-					?>
-				</ul>
-			</div>
-        </div>
+		<div class="row justify-content-center">
+		<?php
+		foreach ( get_field( 'featured_logos', 'options' ) as $l ) {
+			?>
+		<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-3">
+			<?php
+			echo wp_get_attachment_image(
+				$l,
+				'full',
+				false,
+				array(
+					'width'   => '305',
+					'height'  => '90',
+					'loading' => 'eager',
+				)
+			);
+			?>
+		</div>
+			<?php
+		}
+		?>
     </div>
 </section>
-<?php
-add_action(
-	'wp_footer',
-	function () {
-    	?>
-<script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js" nitro-exclude></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css" />
-<script nitro-exclude>
-    document.addEventListener('DOMContentLoaded', function () {
-        new Splide('.featuredSplide', {
-            type: 'loop',
-            autoplay: true,
-            perPage: 5,
-            gap: '20px',
-			arrows: false,
-			pagination: false,
-            breakpoints: {
-				992: {
-					perPage: 4,
-				},
-				768: {
-					perPage: 3,
-				},
-                576: {
-                    perPage: 2,
-                },
-            },
-        }).mount();
-    });
-</script>
-        <?php
-    },
-    9999
-);

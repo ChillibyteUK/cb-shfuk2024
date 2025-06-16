b0afa9872230281128a9ff301fe41f366893042e
ChillibyteUK <dan@chillibyte.co.uk>
2025-06-16 14:27:04 +0100

    single post meta

--- single.php
+++ single.php
@@ -36,7 +36,9 @@ $img = get_the_post_thumbnail(get_the_ID(), 'full', array('class' => 'blog__imag
                 $count = estimate_reading_time_in_minutes( get_the_content(), 200, true, true );
                 ?>
                 <div class="fs-200 mb-4">
-                    Posted by Jack Malnick | <?= esc_html( get_the_date( 'j F, Y' ) ); ?> | Estimated Reading Time '<?= esc_html( $count ); ?>
+                    <i class="fa-solid fa-user"></i> Jack Malnick |
+                    <i class="fa-solid fa-calendar-days"></i> <?= esc_html( get_the_date( 'j F, Y' ) ); ?> |
+                    <i class="fa-solid fa-hourglass"></i> <?= esc_html( $count ); ?>
                 </div>
                 <?php
 

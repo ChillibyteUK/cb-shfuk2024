1e73a2bee631e85611f55bf0bf7d09b3c16d8edf
ChillibyteUK <dan@chillibyte.co.uk>
2025-06-24 10:55:16 +0100

    reviews slider phpcs

--- page-templates/blocks/cb_review_slider.php
+++ page-templates/blocks/cb_review_slider.php
@@ -1,21 +1,34 @@
+<?php
+/**
+ * Review Slider Block Template
+ *
+ * Displays a slider of client reviews.
+ *
+ * @package cb-shfuk2024
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
 <section class="review_slider py-5">
     <div class="container-xl">
         <h2>What our clients say:</h2>
         <div class="review_swiper swiper">
             <div class="swiper-wrapper">
             <?php
-            $q = new WP_Query(array(
-                'post_type' => 'review',
-                'posts_per_page' => -1,
-            ));
-            while ($q->have_posts()) {
+            $q = new WP_Query(
+				array(
+					'post_type'      => 'review',
+					'posts_per_page' => -1,
+				)
+			);
+            while ( $q->have_posts() ) {
                 $q->the_post();
                 ?>
                 <div class="swiper-slide">
                     <div class="review_slide">
-                        <div class="review_title"><?=get_field('review_title',get_the_ID())?></div>
-                        <div class="review"><?=get_field('review',get_the_ID())?></div>
-                        <div class="review_attr"><strong><?=get_the_title(get_the_ID())?></strong> - <?=get_field('reviewer_location',get_the_ID())?></div>
+                        <div class="review_title"><?= esc_html( get_field( 'review_title', get_the_ID() ) ); ?></div>
+                        <div class="review"><?= esc_html( get_field( 'review', get_the_ID() ) ); ?></div>
+                        <div class="review_attr"><strong><?= esc_html( get_the_title( get_the_ID() ) ); ?></strong> - <?= esc_html( get_field( 'reviewer_location', get_the_ID() ) ); ?></div>
                     </div>
                 </div>
                 <?php
